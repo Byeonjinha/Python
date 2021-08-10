922583012e03b624facc757f7bb59d997c37a97b
Byeonjinha <jinhaday@gmail.com>
2021-08-10 18:09:38 +0900

    Ch03엽습문제

--- Ch03/3_2_while.py
+++ Ch03/3_2_while.py
@@ -3,39 +3,60 @@
 이름 : 변진하
 내용 : 파이썬 while문 실습하기 교재 p64
 """
-#1부터 10까지 합
-num1 = 1
-num1 +=1
-tot , k = 0,1
+
+# while
+num = 1
+
+while num < 5:
+    print('%d회 반복' % num)
+    num += 1
+
+# 1부터 10까지 합
+tot, k = 0, 1
+
 while k <= 10:
-    tot+=k
-    k+=1
-print('1부터 10까지 합:', tot)
 
+    tot += k
+    k += 1
 
-#1부터 10까지 짝수합
-total, j = 0,1
-while j <=10:
-     if j % 2 == 0:
-          total += j
-     j+=1
-print('1부터 10까지 짝수합:', total)
-#break
+print('1부터 10까지 합 :', tot)
 
+# 1부터 10까지 짝수합
+total, j = 0, 1
 
-i=1
+while j <= 10:
+
+    if j % 2 == 0:
+        total += j
+
+    j += 1
+
+print('1부터 10까지 짝수합 :', total)
+
+# break
+i = 1
+
 while True:
-    if i%5 ==0 and i%7 == 0:
+
+    if i % 5 == 0 and i % 7 == 0:
         #반복문 종료
         break
-    i+=1
-print('5와7의 최소공배소:',i)
-#continue
-sum,n=0,1
 
-while n <=10:
-    n+=1
-    if n%2==1:
+    i += 1
+
+print('5와 7의 최소공배수 :', i)
+
+# continue
+sum, n = 0, 1
+
+while n <= 10:
+
+    n += 1
+
+    if n % 2 == 1:
+        # 반복문 처음으로 이동
         continue
+
     sum += n
-print('1부터 10까지 짝수합:',sum)
+
+print('1부터 10까지 짝수합 :', sum)
--- Ch03/Ex3.py
+++ Ch03/Ex3.py
@@ -1,34 +1,48 @@
 """
-날짜 : 2021/08/09
+날짜 : 2021/08/10
 이름 : 변진하
-내용 : 파이썬 2장 연습문제 56p
+내용 : 파이썬 3장 연습문제 77p
 """
-
-#1
-su = 5
-dan = 800
-print('su 주소 : ' , id(su))
-print('dan 주소 : ', id(dan))
-print('금액 :' , su*dan)
+import math
+import random
+#1-A
+# x = input("짐의 무게는 얼마입니까?")
+# x = int(x)
+# if x <10:
+#     print("수수료는 없습니다.")
+# else:
+#     print("수수료는 10000원 입니다.")
+#1-B
+# x = input("짐의 무게는 얼마입니까?")
+# x = int(x)
+# if x <10:
+#     print("수수료는 없습니다.")
+# else:
+#     y=x/10
+#     print("수수료는 %d원 입니다." % (int(y)*10000))
 
 #2
-x=2
-y=2.5*x**2+3.3*x+6
-print('2차 방정식 결과 =' ,y )
+# print('>>숫자 맞추기 게임<<')
+# com = random.randint(1,10) #1~10사이 난수 저수 발생
+# while True :
+#     my = int(input('예상 숫자를 입력하시오: ')) #숫자 입력
+#     if my==com:
+#         print('~~ 성공 ~~')
+#         break
+#     elif my>com:
+#         print('더 작은 수 입력')
+#     elif my<com:
+#         print('더 큰 수 입력')
 
 #3
-a = input('지방의 그램을 입력하세요 : ')
-b = input('지방의 그램을 입력하세요 : ')
-c = input('지방의 그램을 입력하세요 : ')
-a = int(a)
-b = int(b)
-c = int(c)
-print('총칼로리 : ',a*9+b*4+c*4,'cal')
+# print("수열 = ", end=" ")
+# sum=0
+# for i in range (1,100):
+#     if i%3==0:
+#         if i%2!=0:
+#             sum+=i
+#             print(i , end=" ")
+#
+# print("\n누적합 = ", sum)
 
 #4
-word1 = input('첫번째 단어 : ')
-word2 = input('두번째 단어 : ')
-word3 = input('세번째 단어 : ')
-print('='*20)
-print('약자 : ' ,word1[0:1]+word2[0:1]+word3[0:1])
-
