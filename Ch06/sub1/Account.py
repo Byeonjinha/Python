c4a43dcc3d87d53b7fb13e697f091232f781c626
Byeonjinha <jinhaday@gmail.com>
2021-08-11 18:18:13 +0900

    2021/08/11 Ch06

--- Ch06/sub1/Account.py
+++ Ch06/sub1/Account.py
@@ -1,22 +1,18 @@
-class Car:
-    def __init__(self, model, color, price):
+class Account:
+    def __init__(self, bank, id, name, balance):
         # 속성
-        self.model = model
-        self.color = color
-        self.price = price
-
-    # 기능
-
-
-def speedUp(self):
-    print('%s speed Up...' % self.model)
-
-
-def speedDown(self):
-    print('%s speed Down' % self.model)
-
-
-def show(self):
-    print('차량명:', self.model)
-    print('차량색:', self.color)
-    print('차량가격:', self.price)
+        self.bank = bank
+        self.id = id
+        self.name = name
+        self.balance = balance
+    #기능
+    def deposit(self, balance):
+        self.balance = balance
+    def withdraw(self, balance):
+        self.balance = balance
+    def show(self):
+        print('---------------------------')
+        print('은행명 : ', self.bank )
+        print('계좌번호', self.id)
+        print('입금주 : ', self.name )
+        print('현재잔액 : ', self.balance)
