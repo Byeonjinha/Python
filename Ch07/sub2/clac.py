441600d1639aaef1ec9640fc396a3f0f4f7230aa
Byeonjinha <jinhaday@gmail.com>
2021-08-13 17:05:52 +0900

    PythonTest2

--- Ch07/sub2/clac.py
+++
@@ -1,9 +0,0 @@
-def plus(x,y):
-    return x+y
-def minus(x,y):
-    return x-y
-def multi(x,y):
-    return x*y
-def div(x,y):
-    return x/y
-
---
+++ CodingTest/Test01.py
@@ -0,0 +1,23 @@
+
+n,m,k = map(int,input().split())
+
+data=list(map(int,input().split()))
+
+
+data.sort()
+
+
+print(n)
+print(m)
+print(k)
+print(data)
+
+
+
+r
+# print(result)
+#
+#
+# [2,4,5,4,6]
+#  M=8
+#  K=3
