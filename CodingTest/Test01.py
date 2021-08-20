2bffcfdd20f50e2604fe3bbe26e88a599206822d
Byeonjinha <jinhaday@gmail.com>
2021-08-20 15:39:58 +0900

    2021/08/20

--- CodingTest/Test01.py
+++ CodingTest/Test01.py
@@ -1,23 +1,14 @@
+t = int(input())
 
-n,m,k = map(int,input().split())
-
-data=list(map(int,input().split()))
-
-
-data.sort()
-
-
-print(n)
-print(m)
-print(k)
-print(data)
-
-
-
-r
-# print(result)
-#
-#
-# [2,4,5,4,6]
-#  M=8
-#  K=3
+for _ in range(t):
+    x, y = map(int,input().split())
+    distance = y - x
+    count = 0  # 이동 횟수
+    move = 1  # count별 이동 가능한 거리
+    move_plus = 0  # 이동한 거리의 합
+    while move_plus < distance :
+        count += 1
+        move_plus += move  # count 수에 해당하는 move를 더함
+        if count % 2 == 0 :  # count가 2의 배수일 때,
+            move += 1
+    print(count)
