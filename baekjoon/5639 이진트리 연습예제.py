4f8794959f79aef836b84b970c0f3c93237d0cdf
Byeonjinha <jinhaday@gmail.com>
2021-08-31 17:27:43 +0900

    2021/08/31

--- baekjoon/5639 이진트리 연습예제.py
+++ baekjoon/5639 이진트리 연습예제.py
@@ -1,44 +1,47 @@
+import sys
+sys.setrecursionlimit(10**6)
 class Node:
-    def __init__(self,data, left_node, right_node):
-        n=0
+    def __init__(self, data, left_node, right_node):
         self.data=data
-        self.left_node = left_node
+        self.left_node= left_node
         self.right_node = right_node
-        n=n+1
 
-
-#전위 순회(Preorder Traversal)
+#전위순회
 def pre_order(node):
     print(node.data, end=' ')
-    if node.left_node !=None:
+    if node.left_node != None:
         pre_order(tree[node.left_node])
-    if node.right_node !=None:
+    if node.right_node != None:
         pre_order(tree[node.right_node])
-#중위 순회(Inorder Traversal)
+
+#중위순회
+
 def in_order(node):
     if node.left_node != None:
         in_order(tree[node.left_node])
-    print(node.data,end=" ")
+    print(node.data, end=' ')
     if node.right_node != None:
         in_order(tree[node.right_node])
+#후위순회
 
-#후위 순회(Postorder Traversal)
 def post_order(node):
     if node.left_node != None:
         post_order(tree[node.left_node])
     if node.right_node != None:
         post_order(tree[node.right_node])
-    print(node.data, end=" ")
-n = int(input())
-tree = {}
+    print(node.data, end=' ')
 
-for i in range(n):
+n = int(input())
+tree ={}
+
+for i  in range(n):
     data, left_node, right_node = input().split()
-    if left_node =="None":
+    if left_node ==  "None":
         left_node = None
-    if right_node =="None":
+    if right_node == "None":
         right_node = None
-    tree[data] =Node(data, left_node, right_node)
+    tree[data] = Node(data, left_node, right_node)
+
 
 pre_order(tree['A'])
 print()
@@ -46,28 +49,6 @@ in_order(tree['A'])
 print()
 post_order(tree['A'])
 
-'''
-예제
-7
-A B C
-B D E
-C F G
-D None None
-E None None
-F None None
-G None None
-'''
+"""
 
-'''
-예제2
-9
-50 30 98
-30 24 45
-24 5 28
-5 None None
-28 None None
-45 None None
-98 52 None
-52 None 60
-60 None None
-'''
+"""
--- baekjoon/9012 괄호.py
+++ baekjoon/9012 괄호.py
@@ -1,33 +1,22 @@
-import sys
-count=int(0)
-def gwalho(T):
-    for i in range(T):
-        gwal= list(sys.stdin.readline())
-        gwal.pop(-1)
-        if len(gwal)%2!=0:      #짝수아니면 NO
-            print("NO")         #
-            continue
-        if gwal[0]==")":        # )로시작하면 NO
-            print("NO")         #
-            continue
+def check_VPS(data):
+    result = []
+    for i in data:
+        if i == '(':
+            result.append(i)
         else:
-            gumsa(gwal)
-def gumsa(gwal):
-    global count
-    count = 0      # count 초기화
-    for i in range(len(gwal)):      #)가 (보다 앞에나오면 No
-        if gwal[i]=="(":            #
-            count=count+1           #
-        elif gwal[i]==")":          #
-            count=count-1           #
-            if count<0:             #
-                print("NO")         #
-                break
-    if count==0:
-        print("YES")    #() 짝의 개수가 맞으면 Yes
-    elif count>0:       # () 가 0을 넘으면 No
-        print("N0")
-    return
+            if len(result) == 0:
+                print('NO')
+                return
+            else:
+                result.pop()
 
-T = int(sys.stdin.readline())
-gwalho(T)
+    if len(result) == 0:
+        print('YES')
+    else:
+        print('NO')
+
+
+n = int(input())
+for i in range(n):
+    parenthesis = input()
+    check_VPS(parenthesis)
