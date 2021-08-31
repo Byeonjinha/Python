45b2facbc44c86e1597917211114ac717a1e8edd
Byeonjinha <jinhaday@gmail.com>
2021-08-31 17:27:06 +0900

    2021/08/31

---
+++ bigdata/4_Selenium.py
@@ -0,0 +1,16 @@
+"""
+날짜 : 2021/08/31
+이름 : 변진하
+내용 : 파이썬 날씨 데이터 크롤링 실습
+"""
+import os
+from datetime import datetime
+
+import requests as req
+from bs4 import BeautifulSoup as bs
+
+'''
+
+https://chromedriver.storage.googleapis.com/index.html?path=92.0.4515.107/ 설치
+
+'''
---
+++ programmers/키패드 누르기.py
@@ -0,0 +1,18 @@
+# keypad = [[0 for _ in range(4)]for _ in range(3)]
+# left=[]
+# right=[]
+# def solution(numbers, hand):
+#     for i in range(0,len(numbers)):
+#         if numbers[i]==1 or numbers[i]==4 or numbers[i]==7:
+#             left.append([i,"L"])
+#         elif numbers[i]==3 or numbers[i]==6 or numbers[i]==9:
+#             right.append([i,"R"])
+#         elif numbers[i]==2 :
+#             a=[]
+#
+#
+# numbers= [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+# hand="right"
+# result=	"LRLLLRLLRRL"
+#
+# solution(numbers, hand)
